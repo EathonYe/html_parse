7b167725cdc66b4c5b29e6618d2ae72cadd31707
eathon <yyye33@yahoo.com>
2020-10-12 21:16:55 +0800

    add examples

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use html_parse;
 use html_parse::parse;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+use std::env;
 
 fn main() {
-    let tree = parse(
-        "<div>
-        <!-- <div>comment</div> -->
-        <img src=\"xxx\" alt=\"yyy\" />
-        <input type=\"text\" />
-        <!-- <div>comment</div> -->
-        <p>
-            This is a p tag.
-            <span>lalala</span>
-        </p>
-    </div>",
-    );
+    // let file = "examples/input";
+    let file = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Please input file path.");
+            process::exit(1);
+        }
+    };
+    let mut buffer = String::new();
+    let mut file = File::open(file).unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    if buffer.is_empty() {
+        eprintln!("input file missing");
+        process::exit(1);
+    }
+    let tree = parse(&buffer);
 
     println!("{:#?}", tree);
 }

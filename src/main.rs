6645bc0a205927d07a55fbdccf9d4c3e7653d349
eathon <yyye33@yahoo.com>
2020-10-16 15:47:58 +0800

    refactor: write result to output file

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,31 @@ use std::env;
 
 fn main() {
     // let file = "examples/input";
-    let file = match env::args().nth(1) {
+    let input_file = match env::args().nth(1) {
         Some(path) => path,
         None => {
-            println!("Please input file path.");
+            println!("No input file path.");
+            process::exit(1);
+        }
+    };
+    let output_file = match env::args().nth(2) {
+        Some(path) => path,
+        None => {
+            println!("No output file path.");
             process::exit(1);
         }
     };
     let mut buffer = String::new();
-    let mut file = File::open(file).unwrap();
-    file.read_to_string(&mut buffer).unwrap();
+    let mut input = File::open(input_file).unwrap();
+    input.read_to_string(&mut buffer).unwrap();
     if buffer.is_empty() {
         eprintln!("input file missing");
         process::exit(1);
     }
     let tree = parse(&buffer);
 
-    println!("{:#?}", tree);
+    // println!("{:#?}", tree);
+    let mut output = File::create(output_file).unwrap();
+    let buffer = format!("{:#?}", tree).into_bytes();
+    output.write(&buffer).expect("failed to write tree into output file.");
 }
